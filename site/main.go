753c65dd51a8e690b8fd3c568cb2b890094c6dab
Melanie Warrick <warrick@google.com>
2019-02-17 22:08:53 -0800

    Experimenting with passing data to and from a from

--- site/main.go
+++ site/main.go
@@ -26,10 +26,24 @@ import (
 type handler struct {
 	hf http.HandlerFunc
 }
-type templateParams struct {
-	Name string
-	Date string
+
+
+type RadioButton struct {
+	Name       string
+	Value      string
+	IsDisabled bool
+	IsChecked  bool
+	Text       string
 }
+
+type templateParams struct {
+	PageTitle       string
+	Date 			string
+	PageRadioButtons []RadioButton
+	Answer          string
+
+}
+
 
 func main() {
 	//Start the web server, set the port to listen to 8080. Without assumes localhost.
@@ -57,28 +71,9 @@ func main() {
 	//Start the web server, set the port to listen to 8080. Without a path it assumes localhost
 	//Print any errors from starting the webserver
 	http.Handle("/", handler{indexHandler})
-
+	http.Handle("/selected", handler{UserSelected})
 	log.Printf("Listening on port %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-}
-
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-
-	params := templateParams {}
-	params.Name = "Flip th Script"
-    params.Date = time.Now().Format("02-01-2006")
-
-	t := template.Must(template.ParseFiles("content/index.html")) //parse the html file index.html
-
-	if err := getBQData(r); err != nil {
-		log.Fatalf("Error getting data: %v\n", err)
-	}
-
-	//execute the template and pass it the params struct to fill in the gaps
-	if err := t.ExecuteTemplate(w, "index.html", params); err !=nil { // if there is an error
-		http.Error(w, err.Error(), http.StatusInternalServerError) }//log it
-
 }
 
 func (h *handler) serveIndex(w http.ResponseWriter, r *http.Request) {
@@ -88,18 +83,58 @@ func (h *handler) serveIndex(w http.ResponseWriter, r *http.Request) {
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	current := r.URL.Path
-/*	w.Header().Set("Content-Type", "text/plain")
-*/	if current == "/" {
+	/*	w.Header().Set("Content-Type", "text/plain")*/
+	if current == "/" || current == "/selected" {
 		h.hf(w, r)
 		return
-	}
-	if current != "/" {
+	} else {
 		http.NotFound(w, r)
 		return
 	}
-/*	err != nil{
-		http.Error(w, "cannot render the page", http.StatusInternalServerError)
-	}*/
+
 }
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+	params := templateParams {}
+	params.PageTitle = "Flip th Script"
+	params.PageRadioButtons = []RadioButton{
+		RadioButton{"animalselect", "cats", false, false, "Cats"},
+		RadioButton{"animalselect", "dogs", false, false, "Dogs"},}
+    params.Date = time.Now().Format("02-01-2006")
 
 
+	t := template.Must(template.ParseFiles("content/index.html"))
+/*	if err := getBQData(r); err != nil {
+		log.Fatalf("Error getting data: %v\n", err)
+	}
+*/
+	//execute the template and pass it the params struct to fill in the gaps
+	if err := t.ExecuteTemplate(w, "index.html", params); err !=nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Print("Form execute error: ", err)
+	}
+}
+/*https://blog.scottlogic.com/2017/02/28/building-a-web-app-with-go.html*/
+func UserSelected(w http.ResponseWriter, r *http.Request){
+	r.ParseForm()
+	// r.Form is now either
+	// map[animalselect:[cats]] OR
+	// map[animalselect:[dogs]]
+	// so get the animal which has been selected
+	youranimal := r.Form.Get("animalselect")
+
+	params := templateParams {}
+	params.PageTitle = "Your preferred animal"
+	params.Answer = youranimal
+
+
+	// generate page by passing page variables into template
+
+	t := template.Must(template.ParseFiles("content/index.html"))
+
+	//execute the template and pass it the params struct to fill in the gaps
+	if err := t.ExecuteTemplate(w, "index.html", params); err !=nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Print("Post execute error: ", err)
+	}
+}
